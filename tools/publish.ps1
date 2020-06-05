21cf1b54ed6a3a11927118db2569244212cd6c03
aevitas <m4rtijn@gmail.com>
2020-06-05 09:50:26 +0200

    Update publish script

--- tools/publish.ps1
+++ tools/publish.ps1
@@ -1,3 +1,3 @@
-dotnet publish ./Patcher.sln -o="./publish/win-x64" -f="netcoreapp3.1" -p:PublishSingleFile=true  -c=Release --runtime win-x64
-dotnet publish ./Patcher.sln -o="./publish/osx-64" -f="netcoreapp3.1" -p:PublishSingleFile=true -c=Release --runtime osx-x64
-dotnet publish ./Patcher.sln -o="./publish/linux-x64" -f="netcoreapp3.1" -p:PublishSingleFile=true -c=Release --runtime linux-x64
+dotnet publish ../src/Patcher.sln -o="./publish/win-x64" -f="netcoreapp3.1" -p:PublishSingleFile=true  -c=Release --runtime win-x64
+dotnet publish ../src/Patcher.sln -o="./publish/osx-64" -f="netcoreapp3.1" -p:PublishSingleFile=true -c=Release --runtime osx-x64
+dotnet publish ../src/Patcher.sln -o="./publish/linux-x64" -f="netcoreapp3.1" -p:PublishSingleFile=true -c=Release --runtime linux-x64
